8fab95a7408a6010e27dded2582b0db60e5eddf9
precious <precious>
2024-07-10 12:41:04 +0100

    bg:responsive fixes on the homepage

--- components/landing/GetStarted.tsx
+++ components/landing/GetStarted.tsx
@@ -5,11 +5,11 @@ const GetStarted = () => {
   return (
     <div className="bg-[#E3EFFC] flex flex-col items-center py-[65px]">
       <div>
-        <h1 className="text-proDark text-center text-[44px] lg:text-[64px] font-bold">
+        <h1 className="text-proDark text-center text-[30px] lg:text-[64px] font-bold">
           Ready to Get Started?
         </h1>
 
-        <div className="flex items-center gap-[16px] justify-center my-[16px]">
+        <div className="flex flex-col lg:flex-row items-center gap-[16px] justify-center my-[16px]">
           <div className="flex gap-1 items-center text-proDark text-sm  font-medium lg:text-base ">
             <Image
               src="/images/svg/phone-icon.svg"
--- components/landing/SubscribeSection.tsx
+++ components/landing/SubscribeSection.tsx
@@ -1,13 +1,13 @@
 const SubscribeSection = () => {
   return (
     <div className="py-[80px]">
-      <div className="w-[90%] mx-auto bg-[#F0F6FF] rounded-[32px] p-[64px]">
-        <div className="max-w-[527px]">
+      <div className="w-[90%] mx-auto bg-[#F0F6FF] rounded-[32px]  p-4 lg:p-[64px]">
+        <div className="lg:max-w-[527px]">
           <div>
-            <h1 className="text-[40px] leading-[48px] font-semibold text-black">
+            <h1 className="text-2xl text-center lg:text-left lg:text-[40px] lg:leading-[48px] font-semibold text-black">
               Enter your email address and get the last updates
             </h1>
-            <p className="my-[16px] text-lg text-[#002966] ">
+            <p className="my-[16px] text-center lg:text-left text-sm lg:text-lg text-[#002966] ">
               Stay up to date with the latest information in the industry, our
               new collections, and special offers!
             </p>
@@ -17,9 +17,9 @@ const SubscribeSection = () => {
               <input
                 type="email"
                 placeholder="Enter Address"
-                className="border border-[#D0D5DD] rounded-[8px] py-[18px] px-[16px] w-full text-[#98A2B3] text-sm"
+                className="border border-[#D0D5DD] rounded-[8px] lg:py-[18px] py-[10px]  px-[8px] lg:px-[16px] w-full text-[#98A2B3] text-sm"
               />
-              <button className="bg-proOrange rounded-[8px] px-[16px] py-[16px] font-semibold text-base text-white">
+              <button className="bg-proOrange rounded-[8px] lg:p-[16px] p-[10px]  font-semibold text-sm lg:text-base text-white">
                 Subscribe
               </button>
             </form>
