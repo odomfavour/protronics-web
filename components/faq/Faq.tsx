956cd6d2f7486dd27ceb141caeb557cf6271314d
precious <precious>
2024-07-15 05:38:55 +0100

    bg:ui adjustments

--- components/faq/Faq.tsx
+++ components/faq/Faq.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import FaqItem from "./FaqItem";
 
 interface FaqProps {
@@ -9,10 +9,22 @@ interface FaqProps {
 }
 
 const Faq: React.FC<FaqProps> = ({ data }) => {
+  const [openIndex, setOpenIndex] = useState<number | null>(null);
+
+  const toggleOpen = (index: number) => {
+    setOpenIndex(openIndex === index ? null : index);
+  };
+
   return (
     <section className="w-full mt-[74px] mb-[100px]">
       {data.map((item, index) => (
-        <FaqItem key={index} question={item.question} answers={item.answer} />
+        <FaqItem
+          key={index}
+          question={item.question}
+          answers={item.answer}
+          isOpen={openIndex === index}
+          onToggle={() => toggleOpen(index)}
+        />
       ))}
     </section>
   );
--- components/faq/FaqItem.tsx
+++ components/faq/FaqItem.tsx
@@ -3,20 +3,21 @@ import { IoIosArrowUp } from "react-icons/io";
 interface FaqItemProps {
   question: string;
   answers: string[];
+  isOpen: boolean;
+  onToggle: () => void;
 }
 
-const FaqItem: React.FC<FaqItemProps> = ({ question, answers }) => {
-  const [isOpen, setIsOpen] = useState(false);
-
-  const toggleOpen = () => {
-    setIsOpen(!isOpen);
-  };
-
+const FaqItem: React.FC<FaqItemProps> = ({
+  question,
+  answers,
+  isOpen,
+  onToggle,
+}) => {
   return (
     <div className="mb-4">
       <button
-        onClick={toggleOpen}
-        className={`px-[32px] focus:bg-[#C6DDF7] w-full flex justify-between items-center rounded-[10px] text-proDark py-[28px] text-left text-xl font-normal ${
+        onClick={onToggle}
+        className={`px-[32px] focus:bg-[#C6DDF7] w-full  h-[80px] flex justify-between items-center rounded-t-[10px] text-proDark py-[28px] text-left text-lg lg:text-xl font-normal ${
           isOpen ? "bg-[#C6DDF7]" : "bg-transparent border border-[#D0D5DD]"
         }`}
       >
@@ -30,9 +31,9 @@ const FaqItem: React.FC<FaqItemProps> = ({ question, answers }) => {
         </span>
       </button>
       {isOpen && (
-        <ul className="text-proDark bg-[#E3EFFC] pt-3  rounded-b-[10px]  pb-[45px] list-disc">
+        <ul className="text-proDark bg-[#E3EFFC] pt-3 text-sm lg:text-lg rounded-b-[10px]  pb-[45px] list-disc">
           {answers.map((item, index) => (
-            <li key={index} className="ml-[32px]">
+            <li key={index} className="ml-[32px] mb-2">
               {item}
             </li>
           ))}
